6ff63ca57f8f4b54264b85ac1d1cfe2850b962d8
devjunku <junku8782@gmail.com>
2021-12-24 13:47:09 +0900

    [Feature/VUE CODE]

--- README.md
+++ README.md
@@ -94,6 +94,63 @@ polygon 태그는 여러 다각형을 표현할 수 있습니다. 본 프로젝�
 
 ---
 
+## 📝 새롭게 학습할 내용
+
+#### 🤔 JSON.stringify
+
+JSON.stringify는 javascript object를 json 형태로 변환합니다. 즉, 클라이언트와 서버간 통신 데이터의 유형의 json파일을 빠르게 만들 수 있습니다. JSON.stringify는 다음과 같은 내용을 담고 있습니다.
+
+```javascript
+JSON.stringify(value, replacer, space)
+```
+
+1. value: JSON 문자열로 변환할 값입니다. 배열, 숫자, 문자, 객체 등이 될 수 있습니다. value는 필수 값입니다.
+
+2. replace: 함수 또는 배열이 될 수 있습니다. 이 때 값이 null이거나 제공되지 않으면, 객체의 모든 속성이 JSON 문자열 결과에 포함됩니다.
+
+- replace의 함수를 통해 JSON 데이터로 변환하고 싶지 않은 유형의 경우 이를 제외시킬 수 있습니다.
+- replace의 배열을 통해 배열의 값과 일치하는 값만 JSON 문자화 됩니다.
+
+3. space: 가독성을 목적으로 json 문자열 출력에 공백이나 문자를 삽입한 형태로 출력됩니다. 이때 `문자열을 입력하면 공백이 아닌 문자열`로 `숫자는 공백의 개수`로 사용됩니다. 
+
+#### 🤔 JSON.parse
+
+JSON.stringify는 자바스크립트 데이터를 json로 바꾸는데 사용했다면, JSON.parse는 json 데이터를 자바스크립트 데이터로 바꾸는데 사용합니다. 사용법은 `JSON.parse(JSON데이터)`로 매우 간단합니다.
+
+#### 🤔 Blob
+
+[MDN](https://developer.mozilla.org/ko/docs/Web/API/Blob)
+
+Blob은 파일의 불변인 미가공 데이터를 나타낸다고 합니다. 텍스트와 이진 데이터로 읽을 수 있으며, [ReadableStream](https://developer.mozilla.org/ko/docs/Web/API/ReadableStream)으로 변환후 관련 메소드를 통해서 데이터를 처리할 수 있습니다.
+
+여기서 File 인터페이스는 사용자 시스템 파일을 지원하기 위해 Blob 인터페이스를 확장한 것으로 File 인터페이스는 모든 Blob 기능을 상속합니다.
+
+Blob을 생성하려면 `Blob()` 생성자를 사용하면 됩니다.
+
+위 프로젝트에서는 파일을 export 하기 위해 text를  `JSON.stringify(app.art)` 통해 만들었으며, 타입이 text이기 때문에, text/plain을 사용했습니다.
+
+MDN 예제를 보고 만들었으며, a 태그를 생성한 이후 해당 링크와 다운로드할 경로 그리고 이를 click하여 다운로드 받는 것 까지 진행했습니다. 
+
+#### 🤔 @keyframes
+
+위 keyframes은 css 애니메이션을 사용하기 위해서 사용했습니다. 본 프로젝트에서는 `hamburger` 추가기능을 클릭했을 때 좀 더 스므스하게 아이콘이 등장하여 사용자 경험을 향상시키기 위해서 사용했습니다
+
+도움말의 경우 transition을 사용하여 이를 스므스하게 페이지가 펼쳐지도록 했습니다.
+
+#### 🤔 createObjectURL & revokeObjectURL
+
+`URL.createObjectURL()` 정적 메서드는 주어진 객체를 가리키는 URL을 [DOMString](https://developer.mozilla.org/ko/docs/Web/API/DOMString)으로 반환합니다. 해당 URL은 자신을 생성한 창의 [document](https://developer.mozilla.org/ko/docs/Web/API/Document)가 사라지면 함께 무효화됩니다.
+
+이때 중요한 것은 URL.createObjectURL()는 항상 새로운 객체를 생성하기 때문에 해당 객체를 사용하고 난 뒤에 URL.revokeObjectURL를 사용하여 URL.createObjectURL을 해제해야 합니다.
+
+본 프로젝트에서는 이를 export 하기 위해서 사용했으며, a태그의 링크를 생성하기 위해 사용했습니다. 특히 만약에 사용하려는 text 객체가 null 값이 아니라면 revokeObjectURL를 사용하여 해당 링크를 제거한 뒤에 다시 생성하는 방식으로 메모리 관리를 수행했습니다.
+
+#### 🤔 event.clientX, event.clientY 등 새로운 이벤트 property
+
+위 이벤트는 그림과 여러 도형을 만들기 위해서 사용자가 누르는 button인 1과 3을 기반으로 그리고 싶은 위치를 판단합니다. 여기서 마우스를 기반으로 1번 버튼은 left, 3번은 마우스의 left와 right를 동시에 누르는 것 입니다. 물론 `해당 이벤트는 explorer에서는 지원되지 않는 기능`입니다.
+
+
+
 ## ⧲ 에러 확인 및 수정
 
 1. DS_Store파일이 자동으로 생성되는 것을 확인했습니다. DS_Store 파일을 .gitignore에서 통제하는 방법은 다음과 같습니다.
@@ -101,6 +158,12 @@ polygon 태그는 여러 다각형을 표현할 수 있습니다. 본 프로젝�
    - `$ find . -name .DS_Store -print0 | xargs -0 git rm --ignore-unmatch -f`를 zsh 또는 bash 창에 입력합니다.
    - echo .DS_Store >> .gitignore를 입력합니다.
    - 이후 add, commit, push를 진행합니다.
+
+2. 다크 테마 바뀔 시, 위 `네브바가 닫히지 않습니다.`
+   - 아직 hamburger 토글을 버튼 이벤트에 따라 바뀌는 함수를 작성하지 않았습니다.
+3. `svg태그 안 컨테이너 배경색이` 바뀌지 않는 것을 확인했습니다.
+   - 해당 css artboard 클래스를 만들지 않아서 생긴 문제였습니다. 해당 클래스에 transition를 추가하여 변경하였습니다.
+4. redo의 경우 한글로 적용할 경우 작동하지 않는 것을 확인했습니다.
 
 
 
--- script.js
+++ script.js
@@ -2,13 +2,24 @@ const app = new Vue({
     el: "#app",
     data() {
       return {
-          art: [],
-          tool: 'pensil',
-          hamburger: false,
-          name: "drawing",
-          help: false,
-          dark: false,
-          openOptions: [],
+        art: [],
+        name: "draw",
+        hamburger: false,
+        help: false,
+        dark: false,
+        openModal: false,
+        openOptions: [],
+        tool: 'pencil',
+        img: '',
+  
+        stroke: "#000000",
+        text: "Hello",
+        fill: "#FFFFFF",
+        strokeWidth: 5,
+        linecap: 'butt',
+  
+        preArt: [],
+        lastArtEle: ""
       }
     },
     methods: {
@@ -16,72 +27,312 @@ const app = new Vue({
             const file = event.target.files[0];
             this.img = URL.createObjectURL(file);
         },
+
         selectTool(tool, event){
-        this.tool = tool;
+            this.tool = tool;
         },
+  
 
-        // 저장 함수
-        saveArt(){
-          this.name = prompt('파일 이름을 입력하세요', this.name);
-          // 이름이 있으면 저장하는 것으로
-          if(this.name)
-          {
-              localStorage.setItem(`art_${this.name}`, JSON.stringify(app.art));
-          }
-        },
-
-        move(event){
-          switch(this.tool)
-          {
-              case 'line': this.drawLine(event); break;
-              case 'circle': this.drawCircle(event); break;
-              case 'rect': this.drawRect(event); break;
-              case 'pencil': this.drawPencil(event); break;
-              case 'eraser': this.drawEraser(event); break;
-              case 'text': this.drawText(event); break;
-              case 'img': this.drawImg(event); break;
-          }
-        },
-        
-        start(event){
-            switch(this.tool)
-            {
-                case 'line': this.startLine(event); break;
-                case 'circle': this.startCircle(event); break;
-                case 'rect': this.startRect(event); break;
-                case 'pencil': this.startPencil(event); break;
-                case 'eraser': this.startEraser(event); break;
-                case 'text': this.startText(event); break;
-                case 'img': this.startImg(event); break;
+        deleteArt() {
+            while(this.art.length >= 40) {
+                this.art.shift()
             }
         },
 
-        // 아이템 오픈
-        showOpen(){
-          this.openModal = !this.openModal;
-          if(this.openModal){
-              for(let i in localStorage)
-              {
-                  if(i.substring(0, 4) === 'art_')
-                  {
-                      this.openOptions.push(i);
-                  }
-              }
-          } 
+        startLine(event) {
+            this.art.push({
+                tool: 'line',
+                x1: event.clientX, y1: event.clientY,
+                x2: event.clientX, y2: event.clientY,
+                stroke: this.stroke,
+                strokeWidth: this.strokeWidth != 0 ? this.strokeWidth : 1,
+                linecap: this.linecap,
+                rotate: 0
+            })
+            this.deleteArt()
+      
         },
+  
+        startRect(event) {
+            this.art.push({
+                tool: 'rect',
+                x: event.clientX, y: event.clientY,
+                width: 0, height: 0,
+                stroke: this.stroke,
+                fill: this.fill,
+                strokeWidth: this.strokeWidth,
+                radius: 0,
+                rotate: 0
+            })
 
-        openArt(event){
-          let key = event.target.value;
-          this.name = key.substring(4, key.length)
-          this.art = JSON.parse(localStorage.getItem(key));      
-      },
-      // 모두 지우기
-      trash(){
-        if(confirm("모두 지우시겠습니까?")){
-            this.art = [];
+    
+            this.deleteArt()
+        },
+  
+        startCircle(event) {
+            this.art.push({
+                tool: 'circle',
+                x: event.clientX, y: event.clientY,
+                stroke: this.stroke,
+                fill: this.fill,
+                strokeWidth: this.strokeWidth,
+                radius: 0
+            })
+    
+            this.deleteArt()
+        },
+  
+        startPencil(event) {
+            this.art.push({
+                tool: 'polyline',
+                points: `${event.clientX},${event.clientY} `,
+                width: this.strokeWidth != 0 ? this.strokeWidth : 1,
+                stroke: this.stroke
+            })
+    
+            this.deleteArt()
+        },
+  
+        startEraser(event) {
+            this.art.push({
+                tool: 'eraser',
+                points: `${event.clientX},${event.clientY} `,
+                width: this.strokeWidth != 0 ? this.strokeWidth : 1
+            })
+    
+            this.deleteArt()
+        },
+  
+        startText(event) {
+            this.art.push({
+                tool: 'text',
+                x: event.clientX, y: event.clientY,
+                text: this.text,
+                size: this.strokeWidth != 0 ? this.strokeWidth : 1,
+                stroke: this.stroke,
+                rotate: 0
+            })
+  
+            this.deleteArt()
+        },
+  
+        startImg(event) {
+            this.art.push({
+                tool: 'img',
+                img: this.img,
+                width: 0,
+                x: event.clientX, y: event.clientY,
+                rotate: 0
+            })
+    
+            this.deleteArt()
+        },
+  
+        drawLine(event) {
+            if(event.buttons == 1 || event.buttons == 3) {
+            let lastLine = this.art[this.art.length - 1]
+            lastLine.x2 = event.clientX
+            lastLine.y2 = event.clientY
+            }
+        },
+    
+        drawCircle(event) {
+            if(event.buttons == 1 || event.buttons == 3){
+                let lastCircle = this.art[this.art.length - 1];
+                let a = Math.abs(lastCircle.x - event.clientX);
+                let b = Math.abs(lastCircle.y - event.clientY);
+                lastCircle.radius = Math.sqrt((a * a) + (b * b));
+            }
+        },
+  
+        drawRect(event) {
+            if (event.buttons == 1 || event.buttons == 3) {
+            let lastRect = this.art[this.art.length - 1]
+            if (event.clientX > lastRect.x && event.clientY > lastRect.y) {
+                lastRect.width = event.clientX - lastRect.x
+                lastRect.height = event.clientY - lastRect.y
+            }
+            }
+        },
+  
+        drawImg(event) {
+            if (event.buttons == 1 && event.buttons == 3) {
+                let lastImg = this.art[this.art.length - 1]
+                if (event.clientX - lastImg.x > 0 && event.clientY - lastImg.y > 0) {
+                    lastImg.width = event.clientX - lastImg.x
+                }
+            }
+        },
+  
+        drawPencil(event) {
+            if (event.buttons == 1 || event.buttons == 3) {
+                let lastLine = this.art[this.art.length - 1]
+                lastLine.points += `${event.clientX},${event.clientY} `
+            }
+        },
+    
+        drawEraser(event) {
+            if (event.buttons == 1 || event.buttons == 3) {
+                let lastLine = this.art[this.art.length - 1];
+                lastLine.points += `${event.clientX},${event.clientY} `;
+            }
+        },
+    
+        drawText(event) {
+            if (event.buttons == 1 && event.buttons == 3) {
+            let text = this.art[this.art.length - 1]
+            text.x = event.clientX
+            text.y = event.clientY
+            }
+        },
+    
+        // 이벤트에 따른 그림 그리기 시작
+        start(event) {
+            switch(this.tool) {
+            case 'line': this.startLine(event); break;
+            case 'circle': this.startCircle(event); break;
+            case 'rect': this.startRect(event); break;
+            case 'pencil': this.startPencil(event); break;
+            case 'eraser': this.startEraser(event); break;
+            case 'text': this.startText(event); break;
+            case 'img': this.startImg(event); break;
+            }
+        },
+    
+        move(event) {
+            switch(this.tool) {
+            case 'line': this.drawLine(event); break;
+            case 'circle': this.drawCircle(event); break;
+            case 'rect': this.drawRect(event); break;
+            case 'pencil': this.drawPencil(event); break;
+            case 'eraser': this.drawEraser(event); break;
+            case 'text': this.drawText(event); break;
+            case 'img': this.drawImg(event); break;
+            }
+        },
+    
+        saveArt() {
+            this.name = prompt('파일 이름을 입력하세요.', this.name)
+            if (this.name) {
+            localStorage.setItem(`drawing_${this.name}`, JSON.stringify(app.art))
+            }
+        },
+    
+        showOpen() {
+            this.openModal = !this.showOpen
+            if (this.openModal) {
+            for (let i in localStorage) {
+                if (i.substring(0, 8) === 'drawing_') {
+                this.openOption.push(i)
+                }
+            }
+            }
+        },
+    
+        openArt(event) {
+            let key = event.tartget.value
+            this.name = key.substring(4, key.length)
+            this.art = JSON.parse(localStorage.getItem(key))
+        },
+    
+        importArt(event) {
+            var fileReader = new FileReader()
+            fileReader.onload = function() {
+            app.art = JSON.parse(fileReader.result)
+            }
+            fileReader.readAsText(event.tartget.value[0])
+        },
+    
+        exportArt() {
+            var textFile = null,
+            makeTextFile = function(text) {
+                var data = new Blob([text],  {type: 'text/plain'})
+                if (textFile !== null) {
+                    window.URL.revokeObjectURL(textFile)
+                }
+                textFile = window.URL.createObjectURL(data)
+                return makeTextFile
+            }
+            var filePath = `host/path/${this.name}.drw`
+            var a = document.createElement('A')
+            a.href = makeTextFile(JSON.stringify(app.art))
+            a.download = filePath.substr(filePath.lastIndexOf('/') + 1)
+            document.body.appendChild(a)
+            a.click()
+            document.body.removeChild(a)
+        },
+    
+        trash() {
+            if (confirm("정말로 모두 지우시겠습니까?")) {
+            this.art = []
+            }
+        },
+    
+        keydown(event) {
+            if(this.art.length > 0) {
+            let last = this.art[this.art.length - 1]
+            switch(this.tool) {
+                case 'rect':
+                event.key == 'ArrowDown' && last.radius > 0 ? last.radius -= 2 : ''
+                event.key == 'ArrowUp' ? last.radius += 2 : ''
+                event.key == 'ArrowLeft' ? last.rotate += 2 : ''
+                event.key == 'ArrowRight' ? last.rotate -= 2 : ''
+                event.key == 'a' ? last.x -= 2 : ''
+                event.key == 'd' ? last.x += 2 : ''
+                event.key == 'w' ? last.y -= 2 : '' 
+                event.key == 's' ? last.y += 2 : ''
+                break
+                case 'img':
+                event.key == 'ArrowLeft' ? last.rotate += 2 : '';
+                event.key == 'ArrowRight' ? last.rotate -= 2 : '';
+                event.key == 'a' ? last.x-= 2 : '';
+                event.key == 'd' ? last.x+= 2 : '';
+                event.key == 'w' ? last.y-= 2 : '';
+                event.key == 's' ? last.y+= 2 : '';
+                break
+                case 'text':
+                event.key == 'ArrowLeft' ? last.rotate += 2 : '';
+                event.key == 'ArrowRight' ? last.rotate -= 2 : '';
+                event.key == 'a' ? last.x-= 2 : '';
+                event.key == 'd' ? last.x+= 2 : '';
+                event.key == 'w' ? last.y-= 2 : '';
+                event.key == 's' ? last.y+= 2 : '';
+                break;
+                case 'circle':
+                event.key == 'a' ? last.x-= 2 : '';
+                event.key == 'd' ? last.x+= 2 : '';
+                event.key == 'w' ? last.y-= 2 : '';
+                event.key == 's' ? last.y+= 2 : '';
+                break;
+                }
+            }
+
+            // undo
+            if((event.key == 'z' && event.ctrlKey) || (event.key == 'ㅋ' && event.ctrlKey)) {
+                this.lastArtEle = this.art.pop()
+            } else {
+                this.lastArtEle = ""
+            }
+            if (this.lastArtEle != "") {
+                this.preArt.push(this.lastArtEle)
+            }
+            
+            // redo 구현에서 event.key == 'z'가 다시 돌아가기를 막았었음
+            // event.key를 살펴보니 'Z'로 보였으며, 이로 수정하니 redo가 실행되었음
+            if ((event.key == 'Z' && event.shiftKey && event.ctrlKey) || (event.key == 'ㅋ' && event.shiftKey && event.ctrlKey)) {
+                console.log(event.key)
+                if (this.preArt.length > 0) {
+                    this.lastArtEle = this.preArt.pop()
+                    if (this.art.length < 40) {
+                        this.art.push(this.lastArtEle)
+                    }
+                }
+            }
+            if(event.key == 's' && event.ctrlKey) {
+                event.preventDefault()
+                this.saveArt()
+            }
         }
-    },
-    },
+    }
 })
-
-document.addEventListener("keydown", app.keydown)
+document.addEventListener("keydown", app.keydown);
